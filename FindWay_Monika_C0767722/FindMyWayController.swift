cb18a98205351bf5c92aeeb58392fa5ceb6c59fb
monikasharma26 <monikasharma26081991@gmail.com>
2020-06-12 00:21:14 -0400

    [monika] Name Chawed for Controller file

---
+++ FindWay_Monika_C0767722/FindMyWayController.swift
@@ -0,0 +1,21 @@
+//
+//  FindMyWayController.swift
+//  FindWay_Monika_C0767722
+//
+//  Created by S@i on 2020-06-12.
+//  Copyright © 2020 S@i. All rights reserved.
+//
+
+import UIKit
+import MapKit
+class FindMyWayController: UIViewController{
+    
+    //Stored Properties
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var zoom: UIStepper!
+    
+    
+    override func viewDidLoad() {
+    }
+}
+
--- FindWay_Monika_C0767722/ViewController.swift
+++ FindWay_Monika_C0767722/ViewController.swift
@@ -2,19 +2,8 @@
 //  ViewController.swift
 //  FindWay_Monika_C0767722
 //
-//  Created by S@i on 2020-06-09.
+//  Created by S@i on 2020-06-12.
 //  Copyright © 2020 S@i. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
+import Foundation
